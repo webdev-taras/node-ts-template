e8db1e973983a9fd4a1076b0d58b57ed49f61682
webdev.taras <webdev.taras@gmail.com>
2023-06-05 13:48:38 +0300

    add jest

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-import { hello } from './hello'
-import hola from './hola'
+import http from "http";
+import { hello } from './hello';
 
-console.log(`say ${hello}`)
-console.log(`di ${hola}`)
+export const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: `say ${hello}`,
+    })
+  );
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000/");
+});
