d17946a32e3ff9d69418933cfe161fc6bbf3be19
webdev.taras <webdev.taras@gmail.com>
2023-06-05 13:41:09 +0300

    get: hello

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import express from "express"
 import { hello } from './hello'
 import hola from './hola'
+
+
+const app = express()
+ 
+app.get("/", function (req, res) {
+  res.send(`say ${hello}`)
+});
+ 
+app.listen(8080)
 
 console.log(`say ${hello}`)
 console.log(`di ${hola}`)
